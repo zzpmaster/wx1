3b57a47980c36ced9a96570ea5ddd29dbaa12ce5
zzp <i_zhongpeng@163.com>
2017-01-25 23:28:51 +0800

    add post detail and template

---
+++ data/posts-data.js
@@ -0,0 +1,23 @@
+var local_database = [{
+    date: 'Jan 13 2017',
+    title: '人工智能与机器学习',
+    post_img: '/images/news/n1.png',
+    author_img: '/images/avatar/avatar_1.png',
+    content: '提到人工智能，就一定离不开机器学习。机器学习中比较火的一个主题——深度学习，已经在人脸识别、图像识别、语音识别，自然语言理解NLP技术、大数据挖掘技术、无人驾驶技术等领域获得了应用。Open AI研究科学家Durk Kingma预测，未来3年，DNN、反向传播、SGD仍将是主要的技术。',
+    view_num: 92,
+    collect_num: 65,
+    id: 1
+}, {
+    date: 'Jan 14 2017',
+    title: '微信开发',
+    post_img: '/images/news/n2.png',
+    author_img: '/images/avatar/avatar_1.png',
+    content: '2017年开年，筹备一年的小程序正式发布，引发了行业不小的地震，其带来的应用程序存在方式与现有App模式大有不同，掀起了技术热潮。无需安装用完即走的“触手可及”概念、多种接入方式等便捷的特点可以预见第三方小程序的市场巨大，微信也成为连接PaaS和SaaS的平台。',
+    view_num: 92,
+    collect_num: 120,
+    id: 2
+}]
+
+module.exports = {
+    postList: local_database
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,7 @@
+// 不能使用绝对路径，只能用相对路径
+// 使用require方法加载js模块文件
+var postData = require('../../data/posts-data.js');
+
 Page({
   data:{
   },
@@ -5,27 +9,17 @@ Page({
     // 生命周期函数--监听页面加载
     console.log("onLoad");
 
-    var posts_content = [{
-      date: 'Jan 13 2017',
-      title: '人工智能与机器学习',
-      post_img: '/images/news/n1.png',
-      author_img: '/images/avatar/avatar_1.png',
-      content: '提到人工智能，就一定离不开机器学习。机器学习中比较火的一个主题——深度学习，已经在人脸识别、图像识别、语音识别，自然语言理解NLP技术、大数据挖掘技术、无人驾驶技术等领域获得了应用。Open AI研究科学家Durk Kingma预测，未来3年，DNN、反向传播、SGD仍将是主要的技术。',
-      view_num: 92,
-      collect_num: 65
-    }, {
-      date: 'Jan 14 2017',
-      title: '微信开发',
-      post_img: '/images/news/n2.png',
-      author_img: '/images/avatar/avatar_1.png',
-      content: '2017年开年，筹备一年的小程序正式发布，引发了行业不小的地震，其带来的应用程序存在方式与现有App模式大有不同，掀起了技术热潮。无需安装用完即走的“触手可及”概念、多种接入方式等便捷的特点可以预见第三方小程序的市场巨大，微信也成为连接PaaS和SaaS的平台。',
-      view_num: 92,
-      collect_num: 120
-    }];
-
     this.setData({
-      'post_key': posts_content
+      'post_key': postData.postList
     });
+    //这种方式已经失效，在较早的版本中可以使用
+    //this.data.post_key = postData.postList;
+  },
+  onPostTap: function(event) {
+    var id = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: 'post-detail/post-detail'
+    })
   },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
